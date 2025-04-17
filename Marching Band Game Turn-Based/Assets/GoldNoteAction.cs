1323b6f04d5a9bd65a4c03c4a0a62b623315ce61
Chaiquoise <summercnielson@utexas.edu>
2025-04-17 11:36:57 -0500

    Can determine letter patterns
    patterns of alphabetical, first two last two, and all the same are now recognized and printed in debug.log

--- Marching Band Game Turn-Based/Assets/GoldNoteAction.cs
+++ Marching Band Game Turn-Based/Assets/GoldNoteAction.cs
@@ -5,16 +5,56 @@ using UnityEngine;
 public class GoldNoteAction : MonoBehaviour
 {
     public GameObject letterManager;
-    public GameObject referenceToSelf;
+
 
     public List<GameObject> buttonList;
+    public List<GameObject> enemiesInScene;
+
+    public List<string> convertedLetters;
 
     public void roundEndAction()
     {
         letterManager.GetComponent<LetterButton>().resetBoard();
-        foreach (GameObject button in buttonList)
+        foreach (GameObject button in buttonList) //converting gameobjects with comparable names depending on letter types (still in order)
         {
             Debug.Log(button.ToString());
+
+
+            if (button.ToString() == "A1 (UnityEngine.GameObject)" || button.ToString() == "A2 (UnityEngine.GameObject)" || button.ToString() == "A3 (UnityEngine.GameObject)" || button.ToString() == "A4 (UnityEngine.GameObject)")
+            {
+                Debug.Log("button being changed to A");
+                convertedLetters.Add("A");
+            }
+            else if (button.ToString() == "E1 (UnityEngine.GameObject)" || button.ToString() == "E2 (UnityEngine.GameObject)" || button.ToString() == "E3 (UnityEngine.GameObject)" || button.ToString() == "E4 (UnityEngine.GameObject)")
+            {
+                Debug.Log("button being changed to E");
+                convertedLetters.Add("E");
+            }
+            else if (button.ToString() == "G1 (UnityEngine.GameObject)" || button.ToString() == "G2 (UnityEngine.GameObject)" || button.ToString() == "G3 (UnityEngine.GameObject)" || button.ToString() == "G4 (UnityEngine.GameObject)")
+            {
+                Debug.Log("button being changed to G");
+                convertedLetters.Add("G");
+            }
+            else
+            {
+                Debug.Log("button being changed from " + button.ToString() + " to C");
+                convertedLetters.Add("C");
+            }
+        }
+
+        //now we need to determine if there are any specific patterns that would create a chord
+        //check first if all notes are the same
+        if ((convertedLetters[0] == convertedLetters[3]) && (convertedLetters[2] == convertedLetters[1]) && (convertedLetters[1] == convertedLetters[3]))
+        {
+            Debug.Log("all the same!");
+        }
+        else if ((convertedLetters[0] == convertedLetters[1]) && (convertedLetters[2] == convertedLetters[3]))
+        {
+            Debug.Log("first two and last two!");
+        }
+        else if ((convertedLetters[0] == "A") && (convertedLetters[1] == "C") && (convertedLetters[2] == "E") && (convertedLetters[3] == "G"))
+        {
+            Debug.Log("Alphabetical!");
         }
 
     }
